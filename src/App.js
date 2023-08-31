0071556f9022ef50de1e77415509e3e709f2d467
Bruno <carlitos078955@gmail.com>
2023-08-31 09:05:39 -0500

    Terminando el proyecto

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Sobre from "./pages/Sobre"
 import Page404 from './pages/Page404';
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Post from './pages/post';
+import Categoria from './pages/Categoria';
 
 function App() {
 
@@ -15,6 +16,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/sobre' element={<Sobre/>}/>
+        <Route path='/posts/:id' element={<Post />} />
+        <Route path='/categoria/:id/*' element={<Categoria />} />
         <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
